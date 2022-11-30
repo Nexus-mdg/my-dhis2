949cefcccec79d396c69c4b5f0c684c700bb47a6
toavina <toavina@fjelltopp.org>
2022-11-30 18:47:29 +0300

    added dhis2 checker

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 import warnings
 
+import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse, PlainTextResponse
@@ -56,6 +57,15 @@ async def get_tomcat_version(request):
     return PlainTextResponse(response)
 
 
+async def test_dhis2_endpoint(request):
+    if os.getenv("ENCRYPTED") == "TRUE":
+        url = "https://localhost:8443/dhis-web-commons/security/login.action"
+    else:
+        url = "http://localhost:8080/dhis-web-commons/security/login.action"
+    response = requests.get(url, verify=False)
+    return PlainTextResponse(str(response.status_code))
+
+
 routes = [
     Route('/', endpoint=welcome_home, methods=['GET']),
     Route('/start', endpoint=start, methods=['GET']),
@@ -63,6 +73,7 @@ routes = [
     Route('/stop', endpoint=stop_tomcat, methods=['GET']),
     Route('/truncate', endpoint=truncate_database, methods=['GET']),
     Route('/version', endpoint=get_tomcat_version, methods=['GET']),
+    Route('/test', endpoint=test_dhis2_endpoint, methods=['GET']),
 ]
 
 app = Starlette(debug=False, routes=routes, on_startup=[main])
