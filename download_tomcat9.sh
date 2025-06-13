6ba5cf3cf310c524afd367e87caaf6cadeba1cdd
toavina <toavina@fjelltopp.org>
2025-06-13 12:51:52 +0300

    Update README with detailed upgrade instructions and fix Tomcat download script

--- download_tomcat9.sh
+++ download_tomcat9.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Download Tomcat 9.0.78 and place it in the shared directory
-wget -O shared/apache-tomcat-9.0.78.tar.gz https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.106/bin/apache-tomcat-9.0.106.tar.gz
+wget -O shared/apache-tomcat-9.0.106.tar.gz https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.106/bin/apache-tomcat-9.0.106.tar.gz
 echo "Downloaded Tomcat 9.0.78 to shared directory"
