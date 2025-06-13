ea9ee0ccb4fe537bc5e573d634c729a4032c67c2
toavina <toavina@fjelltopp.org>
2025-06-13 10:12:04 +0300

    Improve Docker configuration and scripts: refactor Dockerfile, fix entrypoint.sh bugs, add healthcheck to docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM ubuntu:20.04
 
-
 RUN apt -y update && \
     apt install -y locales \
             wget \
@@ -10,69 +9,39 @@ RUN apt -y update && \
             libapr1-dev \
             libssl-dev \
             build-essential \
-            fontconfig
-
+            fontconfig \
+            postgresql-client
 
 # Set the locale
 RUN locale-gen en_US.UTF-8
 RUN locale-gen fr_FR.UTF-8
 RUN update-locale
 
-RUN mkdir /opt/java
-RUN mkdir /opt/java/11
-RUN mkdir /opt/dhis2/
-RUN mkdir /tmp/tomcat-conf
-RUN mkdir /pkg
+# Create directories
+RUN mkdir -p /opt/java/11 \
+    /opt/dhis2/ \
+    /opt/tomcat \
+    /tmp/tomcat-conf \
+    /pkg
 
-
-#RUN wget -O /pkg/openjdk.tar.gz "http://sharer:8000/jdk-11.tar.gz"
-#RUN tar -zxvf /pkg/openjdk.tar.gz -C /opt/java/11
-
-
+# Environment variables
 ENV JAVA11_DIR="/opt/java/11/jdk-11"
-ARG JAVA_HOME="/opt/java/11/jdk-11"
-# Default JAVA_OPTS - can be overridden by docker-compose.yaml
-ENV JAVA_OPTS "-Xmx2000m -Xms1000m -Djavax.servlet.request.encoding=UTF-8 -Dfile.encoding=UTF-8"
-
-# Install Tomcat
-RUN mkdir /opt/tomcat
-#RUN wget -O /opt/tomcat/tomcat.tar.gz "http://sharer:8000/apache-tomcat-8.5.47.tar.gz"
-#RUN tar -zxvf /opt/tomcat/tomcat.tar.gz -C /opt/tomcat
-WORKDIR /
+ENV JAVA_HOME="/opt/java/11/jdk-11"
+ENV JAVA_OPTS="-Xmx2000m -Xms1000m -Djavax.servlet.request.encoding=UTF-8 -Dfile.encoding=UTF-8"
 ENV CATALINA_HOME="/opt/tomcat/apache-tomcat-8.5.47"
-ENV PATH $PATH:$CATALINA_HOME/bin
+ENV PATH="$PATH:$CATALINA_HOME/bin"
+ENV DHIS2_HOME="/opt/dhis2/"
+ENV LD_LIBRARY_PATH="/usr/local/apr/lib"
 
-# APR
-#WORKDIR $CATALINA_HOME/bin/
-#RUN tar -xvzf $CATALINA_HOME/bin/tomcat-native.tar.gz
-#WORKDIR $CATALINA_HOME/bin/tomcat-native-1.2.23-src/native
-#RUN echo $(ls -1 $CATALINA_HOME/bin/tomcat-native-1.2.23-src/native)
-#RUN ./configure && make && make install
-#ENV LD_LIBRARY_PATH=/usr/local/apr/lib
-WORKDIR /
-
-#RUN rm -R $CATALINA_HOME/webapps/*
-
-# Entrypoint file
-ADD entrypoint.sh /tmp
-RUN chmod 755 /tmp/entrypoint.sh
-
+# Copy configuration and scripts
 COPY tomcatconf-https/ /tmp/tomcat-conf-https/
 COPY tomcatconf-http/ /tmp/tomcat-conf-http/
-
-ENV DHIS2_HOME="/opt/dhis2/"
+COPY /policy /policy
+ADD entrypoint.sh /tmp/entrypoint.sh
+RUN chmod 755 /tmp/entrypoint.sh
 
 EXPOSE 8080 8443
 
-# Install postgres client
-RUN apt-get install -y postgresql-client
-
-# Policy
-COPY /policy /policy
-
-# Entrypoint
 WORKDIR /pkg
 ENTRYPOINT ["/tmp/entrypoint.sh"]
-
-# RUN Tomcat
-#CMD ["catalina.sh", "run"]
+CMD ["catalina.sh", "run"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,49 +1,60 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Starting DHIS2 container setup..."
+
+# Download and install JDK
+echo "Installing JDK 11..."
 wget -O /pkg/openjdk.tar.gz "http://sharer:8000/jdk-11.tar.gz"
 tar -zxvf /pkg/openjdk.tar.gz -C /opt/java/11
+
+# Download and install Tomcat
+echo "Installing Tomcat..."
 wget -O /opt/tomcat/tomcat.tar.gz "http://sharer:8000/apache-tomcat-8.5.47.tar.gz"
 tar -zxvf /opt/tomcat/tomcat.tar.gz -C /opt/tomcat
 
-# APR
-WORKDIR $CATALINA_HOME/bin/
-RUN tar -xvzf $CATALINA_HOME/bin/tomcat-native.tar.gz
-WORKDIR $CATALINA_HOME/bin/tomcat-native-1.2.23-src/native
-RUN echo $(ls -1 $CATALINA_HOME/bin/tomcat-native-1.2.23-src/native)
-RUN ./configure && make && make install
-ENV LD_LIBRARY_PATH=/usr/local/apr/lib
-
-rm -R $CATALINA_HOME/webapps/*
-
-
-
-# Only JDK 11 is supported now
+# Setup Java environment
 export JAVA_HOME=$JAVA11_DIR
-PATH=$PATH:"$JAVA_HOME"/bin
+export PATH=$PATH:"$JAVA_HOME"/bin
 
-# JDK 11 has unlimited strength cryptography enabled by default, no need to install JCE policy files
 echo "Using JDK 11 - unlimited strength cryptography is enabled by default"
 
-# setup timezone (create env)
-echo "Setting up timezone to $TIMEZONE"
+# Build and install APR
+echo "Building APR native library..."
+cd $CATALINA_HOME/bin/
+tar -xvzf $CATALINA_HOME/bin/tomcat-native.tar.gz
+cd $CATALINA_HOME/bin/tomcat-native-1.2.23-src/native
+echo "Contents of native directory:"
+ls -1 $CATALINA_HOME/bin/tomcat-native-1.2.23-src/native
+./configure && make && make install
+
+# Clean up Tomcat webapps
+rm -rf $CATALINA_HOME/webapps/*
+
+# Setup timezone (create env)
+echo "Setting up timezone to ${TIMEZONE:-UTC}"
+TIMEZONE=${TIMEZONE:-UTC}
 ln -snf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime && echo "$TIMEZONE" > /etc/timezone
 
+# Build DHIS2 configuration
+echo "Building DHIS2 configuration..."
 declare -a conf=()
 
 [ "${ENCRYPTED}" == "TRUE" ] && conf+=("server.https = on") || conf+=("server.https = off")
 [ "${ENCRYPTED}" == "TRUE" ] && conf+=("server.http.port = 8443") || conf+=("server.http.port = 8080")
 conf+=("analytics.cache.expiration = 3600")
-conf+=("connection.dialect = org.hibernate.dialect.PostgresSQLDialect")
+conf+=("connection.dialect = org.hibernate.dialect.PostgreSQLDialect")
 conf+=("connection.driver_class = org.postgresql.Driver")
 conf+=("connection.schema = update")
 
-if [ -z "${DB_PORT}" ]; then
+# Fix DB_PORT logic (was inverted)
+if [ -n "${DB_PORT}" ]; then
   conf+=("connection.url = jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME")
 else
   conf+=("connection.url = jdbc:postgresql://$DB_HOST/$DB_NAME")
 fi
 
+# Handle credentials
 if [ -f /run/secrets/credentials ]; then
     FILE="/run/secrets/credentials"
     echo "Found credentials file"
@@ -52,30 +63,52 @@ if [ -f /run/secrets/credentials ]; then
     conf+=("connection.password = $(awk 'NR==2{print $1}' ${FILE})")
     conf+=("encryption.password = $(awk 'NR==3{print $1}' ${FILE})")
 else
-    echo -e "\031[37;44mSecret file not found, using random default credentials (dangerous)\033[0m"
-    conf+=("connection.username = dhis")
-    conf+=("connection.password = dhis")
-    conf+=("encryption.password = abcdefghijklmnopqrstuvwxyz0123456789@@")
+    echo -e "\033[37;44mSecret file not found, using environment variables or defaults\033[0m"
+    conf+=("connection.username = ${DB_USER:-dhis}")
+    conf+=("connection.password = ${DB_PASSWORD:-dhis}")
+    conf+=("encryption.password = ${ENCRYPTION_PASSWORD:-abcdefghijklmnopqrstuvwxyz0123456789@@}")
 fi
 
-printf '%s\n' "${conf[@]}" >> /opt/dhis2/dhis.conf
+# Write configuration
+printf '%s\n' "${conf[@]}" > /opt/dhis2/dhis.conf
 chmod 0600 /opt/dhis2/dhis.conf
 
+# Setup SSL/TLS if encryption is enabled
 if [ "${ENCRYPTED}" == "TRUE" ]; then
-  echo "Encryption is set !"
+  echo "Setting up HTTPS encryption..."
   RND=uid-$RANDOM-$RANDOM-$RANDOM-$RANDOM
-  keytool -genkey -keyalg RSA -noprompt -alias "$RND" -dname "CN=localhost, OU=NA, O=NA, L=NA, S=NA, C=NA" -keystore /opt/tomcat/keystore.jks -validity 36500 -storepass "$RND" -keypass "$RND"
-  openssl req -x509 -newkey rsa:4096 -keyout /opt/tomcat/localhost-rsa-key.pem -out /opt/tomcat/localhost-rsa-cert.pem -days 36500 -passout pass:"$RND" -subj "/C=MG/ST=Antananarivo/L=Antananarivo/O=Global Security/OU=IT Department/CN=localhost"
+
+  # Generate keystore
+  keytool -genkey -keyalg RSA -noprompt -alias "$RND" \
+    -dname "CN=localhost, OU=NA, O=NA, L=NA, S=NA, C=NA" \
+    -keystore /opt/tomcat/keystore.jks -validity 36500 \
+    -storepass "$RND" -keypass "$RND"
+
+  # Generate PEM certificates
+  openssl req -x509 -newkey rsa:4096 \
+    -keyout /opt/tomcat/localhost-rsa-key.pem \
+    -out /opt/tomcat/localhost-rsa-cert.pem \
+    -days 36500 -passout pass:"$RND" \
+    -subj "/C=MG/ST=Antananarivo/L=Antananarivo/O=Global Security/OU=IT Department/CN=localhost"
+
+  # Copy HTTPS configuration
   cp -f /tmp/tomcat-conf-https/* "$CATALINA_HOME"/conf/
   sed -i 's/PEMsPassphrase/'"$RND"'/g' "$CATALINA_HOME"/conf/server.xml
 else
-  echo "Encryption is not set !"
+  echo "Using HTTP configuration..."
   cp -f /tmp/tomcat-conf-http/* "$CATALINA_HOME"/conf/
 fi
 
-echo "Downloading DHIS2 warfile at $DHIS2_WARFILE_URL"
-curl  -o "$CATALINA_HOME"/webapps/ROOT.war "$DHIS2_WARFILE_URL"
+# Download DHIS2 WAR file
+if [ -n "$DHIS2_WARFILE_URL" ]; then
+  echo "Downloading DHIS2 warfile from $DHIS2_WARFILE_URL"
+  curl -o "$CATALINA_HOME"/webapps/ROOT.war "$DHIS2_WARFILE_URL"
+else
+  echo "Warning: DHIS2_WARFILE_URL not set, skipping WAR file download"
+fi
 
+echo "Setup complete. Starting application..."
 cd /
 
+# Execute the command passed to the container
 exec "$@"
