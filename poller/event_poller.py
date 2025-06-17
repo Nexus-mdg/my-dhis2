f3decf3d7b0345398664d9134b7b0bc4a2118b3b
toavina <toavina@fjelltopp.org>
2025-06-17 12:25:59 +0300

    tmp

--- poller/event_poller.py
+++ poller/event_poller.py
@@ -15,9 +15,9 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Configuration
-DHIS2_URL = os.getenv('DHIS2_URL', 'https://dhis2:8443')
+DHIS2_URL = os.getenv('DHIS2_URL', 'http://dhis2:8080')
 DHIS2_TOKEN = os.getenv('DHIS2_TOKEN')
-NOTIFICATION_URL = os.getenv('NOTIFICATION_URL', 'http://notifications_rcvr:8080')
+NOTIFICATION_URL = os.getenv('NOTIFICATION_URL', 'http://dhis2_notifications_rcvr:8080')
 POLL_INTERVAL = int(os.getenv('POLL_INTERVAL', '10'))
 REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
 REDIS_KEY = 'dhis2:last_event_timestamp'
